4dc91da227ce7f28b0331c0fefe0396d7617eba7
Kamrul Alam <alamk3026@gmail.com>
2020-08-01 17:10:17 -0400

    reverted changes

--- src/August_1.java
+++ src/August_1.java
@@ -1,20 +1,45 @@
-class Solution {
-    public boolean detectCapitalUse(String word) {
-        Stack<Character> st = new Stack<Character>();
-        for(int i =0; i< word.length(); i++){
-            if(word.charAt(i)>= 65 && word.charAt(i) <=90){
-                st.push(word.charAt(i));
-            }
-        }
-        if(st.size() == word.length()){
-            return true;
-        }
-        else if((word.charAt(0) >=65 && word.charAt(0) <=90) && st.size() ==1){
-            return true;
-        }
-        else if(st.size() ==0){
-            return true;
-        }
-        return false;
-    }
-}
+import java.util.*;
+import java.util.regex.Pattern;
+
+/*
+ * Given a word, you need to judge whether the usage of capitals in it is right or not.
+
+We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Otherwise, we define that this word doesn't use capitals in a right way.
+ */
+
+public class August_1 {
+	
+	/*
+	 * Although I have used regular expressions to make my life easier here,
+	 * this could also be done much simple for faster time complexity in a different
+	 * manner. This is not the fastest implementation you can find.
+	 */
+
+	public static boolean detectCapitalUse(String word) {
+	
+		final String regex1 = "[A-Z][a-z]*";
+		final String regex2 = "[A-Z]*";
+		final String regex3 = "[a-z]*";
+		boolean case1 = Pattern.matches(regex1, word);
+		boolean case2 = Pattern.matches(regex2, word);
+		boolean case3 = Pattern.matches(regex3, word);
+		
+		if (case1 == true) return case1;
+		if (case2 == true) return case2;
+		if (case3 == true) return case3;
+		else return false;
+		
+	}//detectCapitcalUse
+
+	public static void main (String [] args) {
+		
+		System.out.println(detectCapitalUse("Flag"));
+		
+	}//main
+	
+}// August_1
