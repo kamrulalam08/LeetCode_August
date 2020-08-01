42edcd4659e958c951a4305e501a9a952976fa34
m-rahat <56198714+m-rahat@users.noreply.github.com>
2020-08-01 16:46:55 -0400

    Added some comments
    Added comments to describe the code, and also explained how there is a faster way to do this without using regular expressions.

--- src/August_1.java
+++ src/August_1.java
@@ -1,7 +1,24 @@
 import java.util.*;
 import java.util.regex.Pattern;
 
+/*
+ * Given a word, you need to judge whether the usage of capitals in it is right or not.
+
+We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Otherwise, we define that this word doesn't use capitals in a right way.
+ */
+
 public class August_1 {
+	
+	/*
+	 * Although I have used regular expressions to make my life easier here,
+	 * this could also be done much simple for faster time complexity in a different
+	 * manner. This is not the fastest implementation you can find.
+	 */
 
 	public static boolean detectCapitalUse(String word) {
 	
